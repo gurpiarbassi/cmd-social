3428def6b3c1ab61818dba500fe5a0897656fb4e
Gurpiar Bassi <gurpiarbassi@Gurpiars-MacBook-Pro.local>
2015-04-04 13:25:13 +0100

    cached instance of MongoDbFactory to use between other beans

--- src/main/java/com/gurps/cmdsocial/Application.java
+++ src/main/java/com/gurps/cmdsocial/Application.java
@@ -3,10 +3,12 @@ package com.gurps.cmdsocial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -23,14 +25,21 @@ import cz.jirutka.spring.embedmongo.EmbeddedMongoBuilder;
  */
 @ComponentScan
 @EnableAutoConfiguration
-@ConfigurationProperties(prefix = "environments.dev.database")
+@EnableConfigurationProperties
+@Configuration
+@ConfigurationProperties(locations="classpath:application.yml", prefix = "environments.dev.database")
 @EnableMongoRepositories(basePackages = "com.gurps.cmdsocial.persistence")
 
 public class Application {
 
 	private String ip;
+	
 	private int port;
+	
 	private String name;
+	
+	
+	private MongoDbFactory mongoDbFactory;
 
 	public int getPort() {
 		return port;
@@ -62,15 +71,21 @@ public class Application {
 	
 	@Bean(destroyMethod = "destroy")
 	public MongoDbFactory mongoDbFactory() throws Exception {
-		Mongo mongo = new EmbeddedMongoBuilder().version("2.4.5").bindIp(getIp())
-		.port(getPort()).build();
-		return new SimpleMongoDbFactory(mongo, getName());
+		if(mongoDbFactory == null){
+			Mongo mongo = new EmbeddedMongoBuilder()
+	        .version("2.4.5")
+	        .bindIp(getIp())
+	        .port(getPort())
+	        .build();
+			mongoDbFactory = new SimpleMongoDbFactory(mongo, getName());
+		}
+		return mongoDbFactory;
 	}
 	
-	public @Bean MongoOperations mongoTemplate(Mongo mongo) throws Exception {
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
-		return mongoTemplate;
-	}
-	
+	@Bean
+	@DependsOn("mongoDbFactory")
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(this.mongoDbFactory);
+    }
 	
 }
